e88cd307115dbfdde440a993ea7f5656a9074728
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-01 22:08:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,56 +5,47 @@ import subprocess
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 
 bot = Client(
-    "merger",
+    "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6169974916:AAHl_RThcfUk7kgPTUDhwBtMBqNhbMjExOg"
+    bot_token="6361913700:AAE2M_10WumBBFRhxod1dPOUbE2W6CwDNHI"
 )
-
-CHOOSE_UR_LANG = "أرسل باقي الصوتيات بالترتيب ثم اضغط دمج الآن  "
-CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("دمج الآن",callback_data="دمج الآن")],
-]
-
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت دمج الصوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " السلام عليكم أنا بوت قص الصوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
 
-@bot.on_message(filters.private & filters.incoming &  filters.audio | filters.voice  )
+@bot.on_message(filters.private & filters.incoming & filters.video | filters.document  )
 def _telegram_file(client, message):
+
   
-  global user_id
   user_id = message.from_user.id 
   file = message
   global file_path
   file_path = message.download(file_name="./downloads/")
-  with open('list.txt','a') as f:
-      f.write(f"file '{file_path}'' \n")
   filename = os.path.basename(file_path)
+  global realname
   realname, ext = os.path.splitext(filename)
   global mp3file
   mp3file = realname+".mp3"
-  message.reply(
-             text = CHOOSE_UR_LANG,
-             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_LANG_BUTTONS)
-
-        )
-
-@bot.on_callback_query()
-def callback_query(CLIENT,CallbackQuery):
-
-  CallbackQuery.edit_message_text(
-      
-      "جار الدمج"
-  )   
-  subprocess.call(['ffmpeg','-f','concat','-safe','0','-i','list.txt', mp3file,'-y']) 
-  with open(mp3file, 'rb') as f:
-         bot.send_audio(user_id, f)
-  subprocess.call(['unlink',"list.txt"]) 
-  subprocess.call(['unlink',mp3file]) 
-  subprocess.call(['unlink',file_path]) 
+  global mp4file
+  mp4file = realname+".mp4"
+  message.reply_text("الآن أرسل نقطة البداية والنهاية بهذه الصورة \n\n hh:mm:ss/hh:mm:ss",reply_markup=ForceReply(True))
 
 
-       
+@bot.on_message(filters.private & filters.reply)
+async def refunc(client,message):
+   if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply)  :
+          endstart = message.text ;await message.delete()
+          strt, end = os.path.split(endstart);strt_point=strt 
+          end_point = end
+  
+          subprocess.call(['ffmpeg','-i',file_path,'-ss',strt_point,'-to',end_point,'-c','copy',mp4file,'-y'])
+          with open(mp4file, 'rb') as f:
+            await bot.send_video(message.chat.id, f)
+          subprocess.call(['unlink',file_path])
+          subprocess.call(['unlink',mp4file])
+
+        
+
 
 bot.run()
