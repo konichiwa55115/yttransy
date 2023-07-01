3a9c7d226894befe8c73e621bdf38bb3bf60dd49
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-01 21:14:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/Transcriber-Telegram-Bot /kony
+git clone https://github.com/konichiwa55115/audmerg /kony
 cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
