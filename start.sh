27f807b09089907ec607cfb96e9a7592bb88a58f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-01 21:11:58 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/Transcriber-Telegram-Bot /kony
+cd /kony
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+gunicorn app:app & python3 bot.py
