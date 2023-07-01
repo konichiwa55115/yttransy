9ebd2825027c9c62a056315049e5342def8406c0
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-01 23:05:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/vidtrim /kony
+git clone https://github.com/GregorR/rnnoise-models /kony/vidtrim
 cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
