0f7eabf489aaff5f9d048d8e3c276a8e8f6f86f5
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-02 19:14:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,45 +2,29 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import ForceReply
 import subprocess
+import shutil
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 
 bot = Client(
-    "voluemincreaser",
+    "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6178182404:AAHHX9ilOcXiqo4m728-N6pR2QZ70CVi4E4"
+    bot_token="6030811502:AAF0tj9q_2BH1HpmRZLkuvBQttmxYdYFw6o"
 )
-
-
-
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت ضغط الصوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " السلام عليكم أنا بوت قص الصوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
 
-@bot.on_message(filters.private & filters.incoming &  filters.audio | filters.voice  )
+@bot.on_message(filters.private & filters.incoming & filters.text  )
 def _telegram_file(client, message):
+
   
-  global user_id
   user_id = message.from_user.id 
-  file = message
-  global file_path
-  file_path = message.download(file_name="./downloads/")
-  filename = os.path.basename(file_path)
-  realname, ext = os.path.splitext(filename)
-  global mp3file
-  mp3file = realname+".mp3"
-  message.reply(
-            "جار الضغط"
-
-        )
- 
-  subprocess.call(['ffmpeg','-i',file_path,'-b:a','50k',mp3file,'-y']) 
-  with open(mp3file, 'rb') as f:
-         bot.send_audio(user_id, f)
-  subprocess.call(['unlink',mp3file]) 
-  subprocess.call(['unlink',file_path]) 
-
-
-       
-
+  url = message.text
+  
+  message.reply_text("جار التحميل\n\n hh:mm:ss/hh:mm:ss")
+  subprocess.call(['mkdir','downloads'])
+  subprocess.call(['yt-dlp','--extract-audio','--audio-format','mp3','-o','downloads/'+"%(title)s.%(ext)s",url])
+  subprocess.call(['uploadgram','-1001821573758','./downloads/'])
+  shutil.rmtree('./downloads/')
 bot.run()
