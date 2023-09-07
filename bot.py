8f11b12dc1678ea42db8d4c5c78750e5d36e4cda
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-08 00:10:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6030811502:AAF0tj9q_2BH1HpmRZLkuvBQttmxYdYFw6o"
+    bot_token="5714654934:AAEVIR8baWhJcgUOtWeNmrSjvdRfYRiY7tI"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
