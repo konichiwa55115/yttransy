2074c7483ffc67774f6697b7a569daa49f05a22e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-08 00:09:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ def callback_query(CLIENT,CallbackQuery):
         video_title = info_dict.get('title', None) 
         mp3file = video_title.rstrip("|-")
        try :
-        cmd(f'''yt-dlp -ciw  --extract-audio --audio-format mp3  -o "{mp3file}" "{link}"''')
+        cmd(f'''yt-dlp -ciw  --extract-audio --audio-format mp3  -o \"{mp3file}\" "{link}"''')
         with open(f"{mp3file}.mp3", 'rb') as f:
          bot.send_audio(user_id, f,caption=video_title)
         cmd(f'''rm res.txt "{mp3file}.mp3"''' )
