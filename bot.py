cd042d0d6c105d224b94ad97d0a417a695d0f709
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-07 23:53:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,10 +133,10 @@ def callback_query(CLIENT,CallbackQuery):
         video_url = info_dict.get("url", None)
         video_id = info_dict.get("id", None)
         video_title = info_dict.get('title', None) 
-        mp3file = video_title +'.mp3'
+        mp3file = video_title.rstrip("|")
        try :
-        cmd(f'''yt-dlp -ciw  --extract-audio --audio-format mp3  -o "{video_title}" "{link}"''')
-        with open(mp3file, 'rb') as f:
+        cmd(f'''yt-dlp -ciw  --extract-audio --audio-format mp3  -o "{mp3file}" "{link}"''')
+        with open(f"{mp3file}.mp3", 'rb') as f:
          bot.send_audio(user_id, f,caption=video_title)
         cmd(f'''rm res.txt "{video_title}.mp3"''' )
        except FileNotFoundError: 
